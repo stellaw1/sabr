4cd3a7d7776f2ca851a45fff6f476fb08bd51701
stellaw1 <stella01wang@gmail.com>
2023-01-21 12:03:59 -0800

    runs prettier

--- screens/DinerHomeScreen.js
+++ screens/DinerHomeScreen.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-import SearchBar from "react-native-dynamic-search-bar";
+import SearchBar from 'react-native-dynamic-search-bar';
 
 export default function DinerHomeScreen({ navigation }) {
     return (
-      <View>
+        <View>
+            <SearchBar
+                placeholder="Search here"
+                onPress={() => alert('onPress')}
+                onChangeText={(text) => console.log(text)}
+            />
 
-      
-          <SearchBar
-            placeholder="Search here"
-            onPress={() => alert("onPress")}
-            onChangeText={(text) => console.log(text)}
-          />
-          
-          <View style={styles.container}>
-              <Text>Welcome to Diner Home</Text>
-          </View>
+            <View style={styles.container}>
+                <Text>Welcome to Diner Home</Text>
+            </View>
         </View>
     );
 }
@@ -28,7 +26,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    searchbar:{
-      backgroundColor: '#fff'
+    searchbar: {
+        backgroundColor: '#fff'
     }
 });
