7101c74c3dd28b29c328ffc5e0d792c0530fa21d
david <david311@student.ubc.ca>
2023-01-21 11:08:34 -0800

    moved stuff to appnavigator

--- App.js
+++ App.js
@@ -1,25 +1,9 @@
 import * as React from "react";
-// import { View, Text } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import BusinessHomeScreen from "./screens/BusinessHomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import DinerHomeScreen from "./screens/DinerHomeScreen";
-
-const Stack = createNativeStackNavigator();
+import AppNavigator from "./navigation/AppNavigator";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="BusinessHome" component={BusinessHomeScreen} />
-        <Stack.Screen name="DinerHome" component={DinerHomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppNavigator />
   );
 }
 
