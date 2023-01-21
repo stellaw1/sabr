152abb4750718123b51d0c58aa888589cfffc1ff
david <david311@student.ubc.ca>
2023-01-21 14:45:11 -0800

    text for Diner home

--- screens/DinerHomeScreen.js
+++ screens/DinerHomeScreen.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 import SearchBar from "react-native-dynamic-search-bar";
-
+import CarouselCards from "../components/CarouselCards"
+import CarouselCardItem from "../components/CarouselCardItem"
 export default function DinerHomeScreen({ navigation }) {
     return (
-      <View>
-
-      
-          <SearchBar
+      <View style={styles.container}>
+        <SearchBar
             placeholder="Search here"
             onPress={() => alert("onPress")}
             onChangeText={(text) => console.log(text)}
           />
-          
-          <View style={styles.container}>
+
+          <View style={styles.container} >
               <Text>Welcome to Diner Home</Text>
+          </View>
+          <View style={styles.container} >
+              <Text>Restaurant Here</Text>
+          </View>
+          <View style={styles.container} >
+              <Text>Restaurant Here</Text>
+          </View>
+          <View style={styles.container} >
+              <Text>Restaurant Here</Text>
+          </View>
+          <View style={styles.container} >
+              <Text>Restaurant Here</Text>
           </View>
         </View>
     );
@@ -23,8 +34,8 @@ export default function DinerHomeScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
+        flex: 3,
+        // backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
     },
