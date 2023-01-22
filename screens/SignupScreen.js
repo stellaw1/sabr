892420b339a213688a0f7b5e9c9cff898229628c
Sherman Lam <shermlam@students.cs.ubc.ca>
2023-01-22 11:06:27 -0800

    fixing RNEUI

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { firebase } from '../config';
+import { Button } from '@rneui/themed';
 
 
 export default function SignupScreen({ navigation }) {
